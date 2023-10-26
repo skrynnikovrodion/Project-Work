58ea56c3e0db2f852c39360ddb9c71b40048ed22
skrynnikovrodion <skrynnikov.fortech@gmail.com>
2023-10-26 14:33:04 +0300

    1 task com

---
+++ work1.js
@@ -0,0 +1,14 @@
+const sortElements = (arr, direction) => {
+    let newArr = arr.slice(0);
+
+    newArr.sort((a,b) => a - b);
+
+    return newArr;
+  }
+
+  const elements = [6, 43, -6, 3, 0, 5, 2, 7];
+  const sortedArray = sortElements(elements, 'asc');
+  
+  console.log(sortedArray);
+  
+  //Работает
