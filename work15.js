4fbc434e073c430ac888fa950013236e8bc644f3
skrynnikovrodion <skrynnikov.fortech@gmail.com>
2023-10-26 14:50:18 +0300

    task15

---
+++ work15.js
@@ -0,0 +1,19 @@
+const longArray = (length) =>{
+    let arr = [];
+    const string = "x";
+    let text = "";
+
+    for (let i = 0; i < length; i++){
+        
+        text += string;
+
+        arr += text + "\n";
+
+    }
+
+    return arr;
+}
+
+let long = 7;
+
+console.log(longArray(long));
