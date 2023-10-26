8545c8561c67611e681ab54bef382f1a80758e0d
skrynnikovrodion <148752378+skrynnikovrodion@users.noreply.github.com>
2023-10-26 18:27:23 +0300

    work3.js

--- work3.js
+++ work3.js
@@ -2,19 +2,11 @@ const findError = (string) =>{
     let result = '';
     find = string.slice(0,3);
     start_replace = 'abc';
+
+    result = (find === start_replace) ? result = 'www' + string.slice(3) : result = string + 'zzz';
     
-    if (find === start_replace){
-        result = 'www' + string.slice(3);
-
-        return result;
-    }
-    else 
-    result = string + 'zzz';
-
     return result;
 }
 
 const input = 'testabctext';
 console.log(findError(input));
-
-//Работает
