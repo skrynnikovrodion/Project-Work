88a50777d59ddbbcfba36c916775866b113b6a5c
skrynnikovrodion <148752378+skrynnikovrodion@users.noreply.github.com>
2023-10-26 18:36:06 +0300

    work6.js

--- work6.js
+++ work6.js
@@ -1,27 +1,27 @@
+const resultArray = (array) => {
+  const resultObject = {};
+  
+  for (let item of array) {
+
+    const resultValue = resultObject[item.name];
+    if (resultValue) {
+  
+      resultValue.price += item.price;
+    } else {
+  
+      resultObject[item.name] = { ...item };
+    }
+  }
+
+  const result = Object.values(resultObject);
+  
+  return result;
+}
+
 const table = 
 [{name: 'test', price: 200},
 {name: 'test1', price: 300},
 {name: 'test', price: 100},
 {name: 'test', price: 600}];
 
-const resultObject = {};
-
-for (let item of table) {
-  const resultValue = resultObject[item.name];
-  if (resultValue) {
-
-    resultValue.price += item.price;
-  } else {
-
-    resultObject[item.name] = { ...item };
-  }
-}
-
-const resultArray = Object.values(resultObject);
-
-
-console.log(resultArray);
-
-
-
-//Решено
+console.log(resultArray(table));
