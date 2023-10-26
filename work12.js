6edcda282c01ba101e5b5702b934844d3c6beef2
skrynnikovrodion <148752378+skrynnikovrodion@users.noreply.github.com>
2023-10-26 18:51:12 +0300

    work12.js

--- work12.js
+++ work12.js
@@ -1,7 +1,9 @@
 const maxminFunc = (array) =>{
+    
     let KeyMax = 0;
     let KeyMin = 0;
     let save = 0;
+    let newArr = array.slice();
 
     for (let i = 0; i < array.length; i++){
 
@@ -20,8 +22,7 @@ const maxminFunc = (array) =>{
         KeyMin = j;
     
     }
-
-    let newArr = array.slice();
+    
     save = newArr[KeyMax];
     newArr[KeyMax] = newArr[KeyMin];
     newArr[KeyMin] = save;
@@ -32,5 +33,3 @@ const maxminFunc = (array) =>{
 const massiv = [5, 7, 2, 9, 5, 6, 3, 1, 8];
 
 console.log(maxminFunc(massiv));
-
-//Решено
