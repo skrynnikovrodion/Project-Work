4a4867ac3b5c85567a216e625427aabd652d60c4
skrynnikovrodion <148752378+skrynnikovrodion@users.noreply.github.com>
2023-10-27 12:07:35 +0300

    work7.js

--- work7.js
+++ work7.js
@@ -1,14 +1,14 @@
-let test = 
+const testingStringArr = (testString) => {
+let newObj = testString.filter((name, age, country) =>{
+  return Object.values(name, age, country).some((value) => value)
+});
+}
+
+const testArray = 
 [{name: 'test', age: 34, country: 'RF'},
 {name: '', age: null, country: ''},
 {name: 'test1', age: null, country: ''},
 {name: '', age: 12, country: ''},
 {name: '', age: null, country: 'RF'}];
 
-
-let newObj = test.filter((name, age, country) =>{
-  
-  return Object.values(name, age, country).some((value) => value)
-});
-
-console.log(newObj);
+console.log(testingStringArr(testArray));
