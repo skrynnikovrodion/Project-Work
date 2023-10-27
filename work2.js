e8b86d68fd1a96a9e2e7902aa8389f5a44116f0a
skrynnikovrodion <148752378+skrynnikovrodion@users.noreply.github.com>
2023-10-27 11:59:03 +0300

    work2.js

--- work2.js
+++ work2.js
@@ -1,18 +1,16 @@
 const examinationOutput = (string) =>{
-    let newString = '';
-    const stringlong = string.length; 
-    
-    if (string.length > 5){
-        newString = string.slice(0, 3) + string.slice(-3);
-
-        return newString;
-    }else 
-        
-   for (let i = 1; i < long; i++) {
-        newString = newString + string.slice(0,1);
-    }
-
-    return newString;
+  let newString = '';
+  const stringlong = string.length; 
+	
+  if (string.length > 5){
+      newString = string.slice(0, 3) + string.slice(-3);
+      return newString;
+  }else 
+      
+ for (let i = 1; i < long; i++) {
+      newString = newString + string.slice(0,1);
+  }
+  return newString;
 }
 
 const words = 'text';
