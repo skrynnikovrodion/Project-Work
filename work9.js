667b90b0e6436f9a029a1e8a71ac9d7b206cfe9f
skrynnikovrodion <148752378+skrynnikovrodion@users.noreply.github.com>
2023-10-27 12:18:20 +0300

    work9.js

--- work9.js
+++ work9.js
@@ -1,9 +1,11 @@
 const duplicatedValues = (array1, array2) => {
-  const result = [...new Set(number1)].filter(item => number2.includes(item));
+  const newArray = [...new Set(array1)];
+  
+	const result= newArray.filter(item => array2.includes(item));
 
   return result;
 }
 const number1 = [5, 2, 7, 3, 6, 8, 2, 9, 1];
 const number2 = [4, 2, 9, 4, 5, 4];
-
-console.log(duplicatedValues(number1, number2);
+  
+console.log(duplicatedValues(number1, number2));
