2b1b39d0179e4a0e7f6e3399d400805755c9fd90
skrynnikovrodion <148752378+skrynnikovrodion@users.noreply.github.com>
2023-10-27 11:32:58 +0300

    work1.js

--- work1.js
+++ work1.js
@@ -1,10 +1,11 @@
 const sortElements = (arr, direction) => {
-    let newArr = arr.slice(0);
+  let newArr = arr.slice(0);
+  let result = newArr.sort((a,b) => a - b);
 
-    newArr.sort((a,b) => a - b);
-
-    return newArr;
-  }
+  newArr = (direction === 'asc') ? result : newArr = (direction === 'desc') ? result.reverse() : console.log("не верно введены переменные");
+  
+  return newArr;
+}
 
   const elements = [6, 43, -6, 3, 0, 5, 2, 7];
   const sortedArray = sortElements(elements, 'asc');
