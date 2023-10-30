8567aa96d6a9935004608db8e0b48dc50ae793f0
skrynnikovrodion <148752378+skrynnikovrodion@users.noreply.github.com>
2023-10-30 10:05:41 +0300

    work14.js

--- work14.js
+++ work14.js
@@ -1,14 +1,13 @@
 const getParams = (url = window.location) =>{
-    let params = {};
+  let params = {};
     
-    new URL(url).searchParams.forEach(function (val, key) {
+  new URL(url).searchParams.forEach(function (val, location) {
+    params[location] = (params[location] !== undefined) ? 
+    params[location] = (!Array.isArray(params[location])) ? [params[location]] : params[location].push(val)
+    : val;
+  });
 
-        params[key] = (params[key] !== undefined) ? 
-            params[key] = (!Array.isArray(params[key])) ? [params[key]] : params[key].push(val) //if
-            : val;  //else
-    });
-
-    return params;
+  return params;
 }
 
 const string = 'https://underscorejs.org?id=123&limit=5&offset=0';
