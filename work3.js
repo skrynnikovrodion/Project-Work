13589cf03801d0defdad18ffbae40ee67b65b739
skrynnikovrodion <148752378+skrynnikovrodion@users.noreply.github.com>
2023-11-01 16:18:57 +0300

    work3.js

--- work3.js
+++ work3.js
@@ -2,9 +2,10 @@ const replaceText = (string) =>{
   let result = '';
   const exam = string.slice(0,3);
   const start_replace = 'abc';
+  
   result = (exam === start_replace)
-  ? 'www' + string.slice(3) 
-  : string + 'zzz';
+    ? 'www' + string.slice(3) 
+    : string + 'zzz';
 
   return result;
 }
