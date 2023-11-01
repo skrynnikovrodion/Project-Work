88d655d58a718cb91632323afe6b6197bf6ee1bb
skrynnikovrodion <148752378+skrynnikovrodion@users.noreply.github.com>
2023-11-01 17:48:31 +0300

    work14.js

--- work14.js
+++ work14.js
@@ -2,11 +2,11 @@ const getParams = (url = window.location) =>{
   let params = {};
   
   new URL(url).searchParams.forEach((val, location) => {
-    params[location] = (params[location] !== undefined) ? 
-    params[location] = (!Array.isArray(params[location])) ? 
-    [params[location]] :
-    params[location].push(val) :
-    val;
+    params[location] = (params[location] !== undefined) ?      //Проверка на определенность
+    params[location] = (!Array.isArray(params[location])) ?    //если не определен, то идет проверка не является ли массивом params
+    [params[location]] :                                       //если не является массивом, то станет массивом
+    params[location].push(val) :                               //если является массивом, то пушим в массив обьекты
+    val;                                                       //если params определен, то равен val
   });
 
   return params;
