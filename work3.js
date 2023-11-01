d73d309f6f4c8b62af72aca79dc19967b4221d82
skrynnikovrodion <148752378+skrynnikovrodion@users.noreply.github.com>
2023-11-01 16:18:29 +0300

    work3.js

--- work3.js
+++ work3.js
@@ -1,8 +1,7 @@
-const replacementFunc = (string) =>{
+const replaceText = (string) =>{
   let result = '';
   const exam = string.slice(0,3);
   const start_replace = 'abc';
-    
   result = (exam === start_replace)
   ? 'www' + string.slice(3) 
   : string + 'zzz';
