1b89e92f383021703410938a6ee61c6dea9925bc
skrynnikovrodion <148752378+skrynnikovrodion@users.noreply.github.com>
2023-11-02 10:53:35 +0300

    work7.js

--- work7.js
+++ work7.js
@@ -1,6 +1,6 @@
 //	7. Отфильтровать коллекцию, где у свойства есть хоть какое-то значение
-const testingStringArr = (testString) => {
-  const newObj = testString.filter((serchObj) => {
+const filterCollect = (stringArray) => {
+  const newObj = stringArray.filter((serchObj) => {
     const result = Object.values(serchObj).some((value) => value);
   
     return result;
@@ -9,11 +9,11 @@ const testingStringArr = (testString) => {
   return newObj;
 }
 
-const testArray = 
+const array = 
   [{name: 'test', age: 34, country: 'RF'},
   {name: '', age: null, country: ''},
   {name: 'test1', age: null, country: ''},
   {name: '', age: 12, country: ''},
   {name: '', age: null, country: 'RF'}];
 
-console.log(testingStringArr(testArray));
+console.log(filterCollect(array));
