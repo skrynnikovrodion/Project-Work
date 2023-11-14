1e8301684c157dedd3d835acb33df16254eb5660
skrynnikovrodion <148752378+skrynnikovrodion@users.noreply.github.com>
2023-11-14 17:42:19 +0300

    Update work1.js

--- work1.js
+++ work1.js
@@ -1,15 +1,15 @@
 const sortElements = (arr, direction) => {
   let newArr = structuredClone(arr);
-  let result = newArr.sort((a,b) => a - b);
+  newArr.sort((a,b) => a - b);
 
   newArr = (direction === 'asc') 
-    ? result 
-    : result.reverse();
+    ? newArr 
+    : newArr.reverse();
   
   return newArr;
 }
 
-  const elements = [6, 43, -6, 3, 0, 5, 2, 7];
-  const sortedArray = sortElements(elements, 'asc');
+const elements = [6, 43, -6, 3, 0, 5, 2, 7];
+const sortedArray = sortElements(elements, 'asc');
   
-  console.log(sortedArray);
+console.log(sortedArray);
