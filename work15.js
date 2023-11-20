d93d550b6209cbc633b900511bf8cf35def6e9cd
skrynnikovrodion <148752378+skrynnikovrodion@users.noreply.github.com>
2023-11-20 11:02:13 +0300

    Update work15.js

--- work15.js
+++ work15.js
@@ -1,8 +1,7 @@
-const increaseFunc = (length) =>{
+const buildPyramid = (length) => {
   let arr = [];
   let string = '';
-
-  for (let i = 0; i < length; i++){
+  for (let i = 0; i < length; i++) {
 		string += 'x';
     arr.push(string);
   }
@@ -10,8 +9,8 @@ const increaseFunc = (length) =>{
   return arr;
 }
 
-let long = 7;
-const array = increaseFunc(long);
-for(let i = 0; i < array.length; i++){
+const long = 7;
+const array = buildPyramid(long);
+for(let i = 0; i < array.length; i++) {
   console.log(array[i]);
 }
