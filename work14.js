f00afe7e4a128dbc6c0889b7461edb8d4c020223
skrynnikovrodion <148752378+skrynnikovrodion@users.noreply.github.com>
2023-11-20 10:29:01 +0300

    Update work14.js

--- work14.js
+++ work14.js
@@ -1,17 +1,15 @@
 const getParams = (url = window.location) =>{
-  let params = {};
-  
-  new URL(url).searchParams.forEach((val, location) => {
-    params[location] = (params[location] !== undefined) ?      //Проверка на определенность
-    params[location] = (!Array.isArray(params[location])) ?    //если не определен, то идет проверка не является ли массивом params
-    [params[location]] :                                       //если не является массивом, то станет массивом
-    params[location].push(val) :                               //если является массивом, то пушим в массив обьекты
-    val;                                                       //если params определен, то равен val
-  });
+    let params = {};
+    
+    new URL(url).searchParams.forEach((val, location) => {
+      params[location] = (params[location] !== undefined) 
+      ? params[location].push(val) 
+      : val;                                                       
+    });
 
-  return params;
+    return params;
 }
-
-const string = 'https://underscorejs.org?id=123&limit=5&offset=0';
-
-console.log(getParams(string));
+  const string = 'https://underscorejs.org?id=123&limit=5&offset=0';
+  
+  console.log(getParams(string));
+  
