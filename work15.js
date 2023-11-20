f6aaf49243d6f428ace0ed834ca2ab0725426520
skrynnikovrodion <148752378+skrynnikovrodion@users.noreply.github.com>
2023-11-20 10:56:54 +0300

    Update work15.js

--- work15.js
+++ work15.js
@@ -1,16 +1,17 @@
-const increaseArray = (length) => {
+const increaseFunc = (length) =>{
   let arr = [];
   let string = '';
 
-  for (let i = 0; i < length; i++) {
-		string += "x";
+  for (let i = 0; i < length; i++){
+		string += 'x';
     arr.push(string);
-		arr += "\n";
   }
 
   return arr;
 }
 
-const long = 7;
-
-console.log(increaseArray(long));
+let long = 7;
+const array = increaseFunc(long);
+for(let i = 0; i < array.length; i++){
+  console.log(array[i]);
+}
